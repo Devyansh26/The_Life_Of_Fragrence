2ce0e51a48054bb70bb085a4cb4640b22a58a273
Devyansh Aggarwal <devyanshaggarwal19999@gmail.com>
2025-11-03 18:37:11 +0530

    Fix typo in CORS allowed origins URL

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000","https://the-life-of-fragrence.vercel.app"],  # frontend URLs
+    allow_origins=["http://localhost:5173", "http://localhost:3000","https://the-life-of-fragrance.vercel.app"],  # frontend URLs
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
