7b8c30b9094998d75e906f613c0314a897146a9b
Devyansh Aggarwal <devyanshaggarwal19999@gmail.com>
2025-11-03 18:06:27 +0530

    add vercel.json

--- potfolio/services/api.js
+++ potfolio/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000'
+const API_BASE_URL ='https://the-life-of-fragrence.onrender.com'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
